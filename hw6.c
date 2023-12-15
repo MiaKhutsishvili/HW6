e5bd15ccbfb386489b622533eaa587f42ebc5821
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2023-12-15 17:52:51 +0330

    Update hw6.c

--- hw6.c
+++ hw6.c
@@ -16,12 +16,12 @@ int main()
 {
     double a, b, c;
     printf ("Enter a: "); scanf ("%lf", &a);
-    while (!a)
-    {
-        printf ("a cannot be 0. Enter a: ");
-        scanf ("%lf", &a);
-    }
     printf ("Enter b: "); scanf ("%lf", &b);
+    if (!a && !b)
+    {
+        printf ("Error: a & b cannot be 0 at the same time.\n");
+        return 0;
+    }
     printf ("Enter c: "); scanf ("%lf", &c);
     if (solver(a, b, c).delta < 0)
         printf ("This equation has no real roots!\n");
@@ -35,6 +35,12 @@ int main()
 struct eq solver (double a, double b, double c)
 {
     struct eq toSolve;
+    if (!a)
+    {
+        toSolve.delta = 0;
+        toSolve.root1 = -c / b;
+        return toSolve;
+    }
     toSolve.delta = (b * b) - (4 * a * c);
     if (toSolve.delta >= 0)
         toSolve.root1 = (sqrt (toSolve.delta) - b) / (2 * a),
